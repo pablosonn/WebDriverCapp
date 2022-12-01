0c5a0b385d673a7f4a217655933178aaa5269219
pablosonn <pa.meliqueo@duocuc.cl>
2022-12-01 14:41:37 -0300

    aa

--- DriverCapp/views.py
+++ DriverCapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from mobi.decorators import detect_mobile
 
 # Create your views here.
 
-
+@detect_mobile
 def PagPrincipal(request):
-  
+    if request.mobile:
+        IsMovile = True
+    else:
+        IsMovile = False
+        #Hacer cosas para otros dispositivos
+    print(IsMovile)
     return render(request, 'PagPrincipal.html')
